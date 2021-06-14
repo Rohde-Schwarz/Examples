e5fdcd10401b94dbf332f18e8ecdabdb82d4e6d0
Miloslav Macko <Miloslav.Macko@rohde-schwarz.com>
2021-06-14 12:33:50 +0200

    HMP4k example

--- GeneralExamples/Python/RsInstrument/RsInstrument_HMP4k_Example.py
+++ GeneralExamples/Python/RsInstrument/RsInstrument_HMP4k_Example.py
@@ -1,11 +1,11 @@
 # Simple example on how to use the RsInstrument module for remote-controlling yor VISA instrument
 # Preconditions:
-# - Installed RsInstrument Python module Version 1.12.1.60 or newer from pypi.org
+# - Installed RsInstrument Python module Version 1.13.0.63 or newer from pypi.org
 # - Installed VISA e.g. R&S Visa 5.12.x or newer
 
 from RsInstrument import *  # The RsInstrument package is hosted on pypi.org, see Readme.txt for more details
 
-RsInstrument.assert_minimum_version('1.12.1.60')
+RsInstrument.assert_minimum_version('1.13.0.63')
 instr = RsInstrument('TCPIP::10.205.0.41::5025::SOCKET', True, True)
 
 idn = instr.query_str('*IDN?')
@@ -14,7 +14,8 @@ print(f'RsInstrument driver version: {instr.driver_version}')
 print(f'Visa manufacturer: {instr.visa_manufacturer}')
 print(f'Instrument full name: {instr.full_instrument_model_name}')
 print(f'Instrument options: {instr.instrument_options}')
-print(f'Instrument options: {instr.instrument_options}')
+instr.visa_timeout = 2000
+
 
 # Close the session
 instr.close()
