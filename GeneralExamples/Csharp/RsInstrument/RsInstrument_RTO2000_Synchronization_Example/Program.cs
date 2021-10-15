ad18ab7a384c3d1fcc9b78d7b9ff6d2c7e33fde0
Miloslav Macko <Miloslav.Macko@rohde-schwarz.com>
2021-10-15 09:23:57 +0200

    RsInstrument C# Version 1.14.0 Examples

--- GeneralExamples/Csharp/RsInstrument/RsInstrument_RTO2000_Synchronization_Example/Program.cs
+++ GeneralExamples/Csharp/RsInstrument/RsInstrument_RTO2000_Synchronization_Example/Program.cs
@@ -20,7 +20,7 @@ namespace RsInstrument_RTO2000_Synchronization_Example
         static void Main()
         {
             RsInstrument instr;
-            RsInstrument.AssertMinVersion("1.13.0");
+            RsInstrument.AssertMinVersion("1.14.0");
 
             try // Separate try-catch for scope initialization prevents accessing uninitialized object
             {
