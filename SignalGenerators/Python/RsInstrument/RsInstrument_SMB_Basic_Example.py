45ca7ed952a9b5fab82853d7f8c490b6844e6311
Miloslav Macko <miloslav.macko@rohde-schwarz.com>
2024-06-13 14:01:57 +0200

    Added basic SMB100A example for Python

---
+++ SignalGenerators/Python/RsInstrument/RsInstrument_SMB_Basic_Example.py
@@ -0,0 +1,23 @@
+"""
+# GitHub examples repository path: SignalGenerators/Python/RsInstrument
+
+This is a Hello-World example for communicating with your SMBxxx instrument.
+Sets Output Frequency and Power, then sets the output state to ON.
+"""
+
+# RsInstrument package is hosted on pypi.org
+from RsInstrument import *
+
+# Initialize the session
+smb = RsInstrument('TCPIP::10.102.52.42::hislip0')
+
+print(f"\nHello, I am: '{smb.idn_string}'")
+print(f'Instrument installed options: {",".join(smb.instrument_options)}')
+
+# Enter your code here...
+smb.write('SOURce:FREQuency:CW 2200000000')
+smb.write('SOURce:POWer:POWer -25.5')
+smb.write('OUTPut:STATe ON')
+
+# Close the session
+smb.close()
