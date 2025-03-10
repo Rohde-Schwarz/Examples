10d7171398d42ef335024fd70b1d1585611304e4
Miloslav Macko <miloslav.macko@rohde-schwarz.com>
2025-03-10 16:29:44 +0100

    Added C# examples for HMC8012 and HMC804x

---
+++ Misc/Csharp/RsInstrument/RsInstrument_HMC8012_Basic_Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RohdeSchwarz.RsInstrument; // Nuget Package, install it through the Nuget Package Manager
+
+// Basic C# example for HMC8012 - setting of the DC Voltage measurement, and reading one value.
+// Please adjust the IP Address to fit your instrument.
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hmc = new RsInstrument("TCPIP::10.102.100.49::INSTR", false, false, "VxiCapable = OFF, AddTermCharToWriteBinData = True");
+            Console.WriteLine("Hello, I am " + hmc.Identification.IdnString);
+            hmc.Write("CONF:VOLT:DC AUTO,0.01"); // DC Voltage measurement.
+            var meas = hmc.Query("READ?"); // Read the result
+            Console.WriteLine($"Measured DC Voltage: {meas} V");
+            hmc.Dispose();
+
+            Console.WriteLine("Press any key to finish...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Powersupplies/Csharp/RsInstrument/RsInstrument_HMC804x_Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using RohdeSchwarz.RsInstrument; // Nuget Package, install it through the Nuget Package Manager
+
+// Basic C# example for HMC804x - setting of the Voltage and Current, and switching the output ON.
+// Please adjust the IP Address to fit your instrument.
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hmc = new RsInstrument("TCPIP::10.102.100.61::INSTR", false, false, "VxiCapable = OFF, AddTermCharToWriteBinData = True");
+            Console.WriteLine("Hello, I am " + hmc.Identification.IdnString);
+            hmc.Write("INSTrument:Select 1"); // Select channel 1
+            hmc.Write("VOLTage 3"); // DC Voltage is 3 V
+            hmc.Write("CURRent 1"); // Current set to 1 A
+            hmc.Write("OUTPut:CHANnel:STATe 1"); // Set CH1 to active state
+            hmc.Write("OUTPut:MASTer:STATe 1"); // Set Main output to active state
+            hmc.QueryOpc();
+            hmc.Dispose();
+
+            Console.WriteLine("Channel 1: Voltage and Curent set, output is ON.");
+            Console.WriteLine("Press any key to finish...");
+            Console.ReadKey();
+        }
+    }
+}
